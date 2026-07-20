a5548c601519439d52d5abb4923f3e97bcbf3c13
Daiiruin <95864317+Daiiruin@users.noreply.github.com>
2026-07-20 09:53:25 +0200

    feat(notes) adding algo for SOAP format (#13)

--- apps/api/src/features/notes/dtos/responses/note-process.response.dto.ts
+++ apps/api/src/features/notes/dtos/responses/note-process.response.dto.ts
@@ -1,6 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 
+class SoapNoteDto {
+  @ApiProperty() declare subjective: string;
+  @ApiProperty() declare objective: string;
+  @ApiProperty() declare assessment: string;
+  @ApiProperty() declare plan: string;
+  @ApiProperty() declare other: string;
+}
+
 export class NoteProcessResponseDto {
   @ApiProperty() declare session_id: string;
   @ApiProperty() declare processed_text: string;
+  @ApiProperty() declare soap_note: SoapNoteDto;
 }
