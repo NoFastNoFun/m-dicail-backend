2bb67d9c8157be982643bc880dc33033fbedad27
Arsène DOBROVOLSKYY <arsened.devweb@gmail.com>
2026-07-21 10:14:04 +0200

    feat(auth) new jwt refresh and logout

--- apps/api/src/features/notes/constants/soap-regex.constants.ts
+++ apps/api/src/features/notes/constants/soap-regex.constants.ts
@@ -7,7 +7,7 @@ export const SOAP_REGEX_RULES: { pattern: RegExp; section: SoapSection }[] = [
   { pattern: /il\s+va\s+falloir\s+lui/i, section: 'plan' },
   { pattern: /lui\s+(prescrire|recommander|conseiller|proposer)/i, section: 'plan' },
   { pattern: /il\s+(faut|faudra|doit|devra)\s+(faire|réaliser|effectuer)/i, section: 'plan' },
-  { pattern: /(flexion|extension|rotation|abduction|adduction|inclinaison)\s*[:\-]?\s*\d+/i, section: 'objective' },
+  { pattern: /(flexion|extension|rotation|abduction|adduction|inclinaison)\s*[:-]?\s*\d+/i, section: 'objective' },
   { pattern: /testing\s+.{0,30}\d\s*(sur|\/)\s*5/i, section: 'objective' },
   { pattern: /il\s+s['']agit/i, section: 'assessment' },
   { pattern: /depuis\s+\d+/i, section: 'subjective' },
