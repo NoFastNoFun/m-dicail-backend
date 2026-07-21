09b99a686458c3b4f5aaa6987e6140dda170635a
Ethan L <ethan@xpwave.io>
2026-07-21 12:26:12 +0200

    oops

--- apps/api/src/migrations/index.ts
+++ apps/api/src/migrations/index.ts
@@ -3,9 +3,4 @@ import { Appointments1750000001000 } from './1750000001000-Appointments';
 import { AddRefreshTokenToUser1751000000000 } from './1751000000000-AddRefreshTokenToUser';
 import { AddRoleToUsers1782811334916 } from './1782811334916-AddRoleToUsers';
 
-export const migrations = [
-  Init1750000000000,
-  Appointments1750000001000,
-  AddRefreshTokenToUser1751000000000,
-  AddRoleToUsers1782811334916,
-];
+export const migrations = [Init1750000000000, Appointments1750000001000, AddRefreshTokenToUser1751000000000, AddRoleToUsers1782811334916];
